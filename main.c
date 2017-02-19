964a583fd56aea27e8e7402545fbc7dd737caeb9
Colin Dignazio <colin.dignazio@gmail.com>
2017-02-19 15:58:04 -0600

    Added single click to reset brew and double click to cancel brew

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 
 //times are all in ms
 #define BLINK_TOGGLE 500
-#define SHORT_PRESS 50
+#define SHORT_PRESS 100
 #define LONG_PRESS 1000
+#define DOUBLE_CLICK_TIME 500
 
 #define BREW_TIME_ESPRESSO 3000
 #define BREW_TIME_LATTE 5000
@@ -72,7 +73,34 @@ void EnableTimerInterrupt() {
   NVIC_Init(&nvicStructure);
 }
 
-int getPinForCoffeeType(Coffee type) {
+// customize the EXTI for button 
+void InitEXTI()
+{
+	EXTI_InitTypeDef EXTI_InitStructure; // External interrupt
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+  // Selects the GPIOA pin 0 used as external interrupt source
+  SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    
+  EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+  EXTI_Init(&EXTI_InitStructure);
+}
+
+// NVCI : Nested Vectored Interrupt Controller for EXTI in IRQ
+void EnableEXTIInterrupt()
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+uint16_t getPinForCoffeeType(Coffee type) {
 	switch(type) {
 		case ESPRESSO:
 			return GPIO_Pin_12;
@@ -87,7 +115,7 @@ int getPinForCoffeeType(Coffee type) {
 	}
 }
 
-int getBrewTimeForCoffeeType(Coffee type) {
+uint16_t getBrewTimeForCoffeeType(Coffee type) {
 	switch(type) {
 		case ESPRESSO:
 			return BREW_TIME_ESPRESSO;
@@ -125,42 +153,107 @@ void initializeSelection() {
 static uint16_t brew_timer = 0;
 static uint16_t button_timer = 0;
 static uint8_t brewing = 0;
+static uint8_t dblclick = 0;
+
+static uint16_t double_click_timer = 0;
+
+void brew() {
+	brew_timer++;
+	if(brew_timer % BLINK_TOGGLE == 0) {					
+		GPIO_ToggleBits(GPIOD, getPinForCoffeeType(selected));
+	}
+			
+	if(brew_timer > getBrewTimeForCoffeeType(selected)) {
+		alertBrewComplete();
+		brewing = 0;
+		brew_timer = 0;
+		GPIO_SetBits(GPIOD, getPinForCoffeeType(selected));
+	}
+}
+
+uint8_t isShortClick() {
+	return button_timer > SHORT_PRESS;
+}
+
+uint8_t isLongClick() {
+	return button_timer > LONG_PRESS;
+}
+
+uint8_t isDoubleClick() {
+	return dblclick && double_click_timer < DOUBLE_CLICK_TIME;
+}
+
+uint8_t button_pressed = 0;
+
+void EXTI0_IRQHandler()
+{
+	uint8_t button_pin;
+	
+  // Checks whether the interrupt from EXTI0 or not
+  if (EXTI_GetITStatus(EXTI_Line0) != RESET) {  
+		button_pin = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);	
+		
+		if(button_pin) {
+			button_pressed = 1;
+		}
+		else {
+			button_pressed = 0;
+		}
+			
+    // Clears the EXTI line pending bit
+    EXTI_ClearITPendingBit(EXTI_Line0);            
+  }
+}
+
+void longClickHandler() {
+	if(!brewing) {
+		brewing = 1;
+	}	
+	button_timer = 0;
+}
+
+void doubleClickHandler() {
+	brewing = 0;
+	dblclick = 0;
+	GPIO_SetBits(GPIOD, getPinForCoffeeType(selected));
+}
+
+void shortClickHandler() {
+	if(!brewing) {
+		changeSelection();
+	} else {
+		if(isDoubleClick()) {
+			doubleClickHandler();
+		} else {
+			brew_timer = 0;
+		}
+		double_click_timer = 0;
+		dblclick = 1;
+	}
+	button_timer = 0;			
+}
 
 /*
  * Handler that gets triggered every time the timer tickers (every 1ms in our case)
  * Only difference between this and an infinite loop is that we can control how often this gets run.
  */
-void TIM2_IRQHandler() {
-	uint8_t button_pin;
-	
+void TIM2_IRQHandler() {	
   if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
     TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 		
-		button_pin = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);	
+		if(dblclick)
+			double_click_timer++;
 		
-		if(!brewing) {
-			if(button_pin) {
-				button_timer++; //keep track of how long the button has been pressed for
-												//I think theres a better way to do this using more interupts
-			} else if(button_timer > LONG_PRESS) {
-				brewing = 1;
-				button_timer = 0;
-			} else if(button_timer > SHORT_PRESS) {
-				changeSelection();
-				button_timer = 0;
-			}
-		}	else {
-			brew_timer++;
-			if(brew_timer % BLINK_TOGGLE == 0) {					
-				GPIO_ToggleBits(GPIOD, getPinForCoffeeType(selected));
-			}
-			
-			if(brew_timer > getBrewTimeForCoffeeType(selected)) {
-				alertBrewComplete();
-				brewing = 0;
-				brew_timer = 0;
-				GPIO_SetBits(GPIOD, getPinForCoffeeType(selected));
-			}
+		if(button_pressed) {
+			button_timer++;
+		} else if(isLongClick()) {
+			longClickHandler();
+		} else if(isShortClick()) {
+			shortClickHandler();
+		}
+		
+		if(brewing) {
+			brew();
 		}
 	}
 }
@@ -169,9 +262,10 @@ int main() {
   InitializeLEDs();
 	InitButton();
   InitializeTimer();
-	
+	InitEXTI();
 	initializeSelection();
 	
+	EnableEXTIInterrupt();
 	EnableTimerInterrupt();
 
   while(1);
